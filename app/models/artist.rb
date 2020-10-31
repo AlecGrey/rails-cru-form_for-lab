1d5622912fac86e3bdba4f717594c9c8996833cc
AlecGrey <alecgrey.coding@gmail.com>
2020-10-31 15:57:42 -0700

    routes and model associations drawn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,6 @@
 class Artist < ApplicationRecord
+
+    has_many :songs
+    has_many :genres, through: :songs
+
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,6 @@
 class Genre < ApplicationRecord
+
+    has_many :songs
+    has_many :artists, through: :songs
+
 end
